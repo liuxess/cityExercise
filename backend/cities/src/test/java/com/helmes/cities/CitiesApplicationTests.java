c420b19260676503541fd1828c9ff0cf30c06022
Lukas <lukas.jankauskas@sage.com>
2023-01-30 12:33:41 +0200

    Init commit

---
+++ backend/cities/src/test/java/com/helmes/cities/CitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helmes.cities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class CitiesApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
